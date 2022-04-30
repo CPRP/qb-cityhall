7b3b8a235c32201671d35026764d888868b231d1
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-04-30 04:30:00 -0400

    removed ped at school

--- config.lua
+++ config.lua
@@ -64,15 +64,16 @@ Config.Peds = {
             debugPoly = false
         }
     },
-     Driving School Ped
-    {
-        model = 'a_m_m_eastsa_02',
-        coords = vec4(0, 0, 0, 0),
-        scenario = 'WORLD_HUMAN_STAND_MOBILE',
-        drivingschool = true,
-        zoneOptions = { -- Used for when UseTarget is false
-            length = 3.0,
-            width = 3.0
-        }
-    }
 }
+--     --Driving School Ped
+--     {
+--         model = 'a_m_m_eastsa_02',
+--         coords = vec4(240.91, -1379.2, 32.74, 138.96),
+--         scenario = 'WORLD_HUMAN_STAND_MOBILE',
+--         drivingschool = true,
+--         zoneOptions = { -- Used for when UseTarget is false
+--             length = 3.0,
+--             width = 3.0
+--         }
+--     }
+-- }
